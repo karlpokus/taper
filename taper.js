fedabc80580a9634f45a54c931d09036792c9ebd
Karl Pokus <karl.pokus@gmail.com>
2017-05-03 17:01:28 +0000

    add almostEqual and hacky test summary

--- taper.js
+++ taper.js
@@ -1,3 +1,5 @@
+var allTestData = [];
+
 function Test(name) {
   this.name = name;
   this.data = []; // v: ok || not ok, name: string
@@ -31,6 +33,14 @@ Test.prototype.equal = function(x, y, name) {
   });
 }
 
+// almostEqual
+Test.prototype.almostEqual = function(x, y, name) {
+  this.data.push({
+    v: (x == y)? 'ok': 'not ok',
+    name: setName(name)
+  });
+}
+
 // pass
 Test.prototype.pass = function(name) {
   this.data.push({
@@ -58,13 +68,53 @@ Test.prototype.throws = function(fn, regex, name) {
   });
 }
 
-// end
-Test.prototype.end = function() {
-  console.log('\n# ' + this.name);
-  this.data.forEach(function(o){
+// printer
+function printer(data) {
+  console.log('\n# ' + data.name);
+  data.data.forEach(function(o){
     console.log(o.v, o.name);
   });
 }
+
+// counter
+function calcSummary(data) {
+  function filterByValue(v, obj) {
+    return obj.v === v;
+  }
+  
+  return data
+    .map(function(obj){
+      return obj.data;
+    })
+    .reduce(function(acc, arr){
+      acc.pass += arr.filter(filterByValue.bind(null, 'ok')).length;
+      acc.fail += arr.filter(filterByValue.bind(null, 'not ok')).length;
+      
+      return acc;
+    }, {pass: 0, fail: 0});
+}
+
+// summary
+function summary() {
+  allTestData.forEach(printer);
+  
+  // total hack. Have to stop now.
+  var sum = calcSummary(allTestData);
+  console.log('\n# summary');
+  console.log('pass', sum.pass);
+  console.log('fail', sum.fail);
+}
+
+// end
+Test.prototype.end = function() {
+  allTestData.push({
+    data: this.data,
+    name: this.name
+  });
+}
+
+// beforeExit allows async, exit does not
+process.on('beforeExit', summary);
 
 module.exports = function(name, cb) {
   if (name && !cb) {
--- tests.js
+++ tests.js
@@ -1,18 +1,17 @@
 var taper = require('./taper'),
     lib = require('./lib');
 
-taper('test 1', function(t){
-  t.ok(lib.returnSomething(true)); // no name
-  t.notOk(null, 'null is not ok')
-  t.equal(lib.returnSomething(4), 4, 'it is 4');
-  t.pass('just a pass');
+taper('Complete API test', function(t){
+  t.ok(lib.returnSomething(true), '.ok');
+  t.notOk(null, '.notOk')
+  t.equal(lib.returnSomething(4), 4, '.equal');
+  t.almostEqual(lib.returnSomething(5), "5", '.almostEqual')
+  t.throws(lib.goBang, /BANG/, '.throws');
+  t.pass('.pass');
   t.end();
 });
 
 taper(function(t){ // no name
-  t.equal(typeof lib.returnSomething(function(){}), 'function', 'it is a fn');
-  t.ok('foo', 'foo is ok');
-  t.pass(); // no name
-  t.throws(lib.goBang, /BANG/, 'goBang throws');
+  t.pass('taper called anonymously');
   t.end();
 });
